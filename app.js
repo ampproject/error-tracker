a86ecf54d108a43c4aef506863ba7564d7cf7087
Raphael Junior <rigwanzojunior@google.com>
2017-06-08 14:46:04 -0400

    Go to JS server conversion.

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('winston');
 var Log = require('@google-cloud/logging');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
-var users = require('./routes/users');
+var errorTracker = require('./routes/errortracker');
 
 
-
-//activate google cloud trace when in production
 if(process.env.NODE_ENV === 'production'){
   require('@google/cloud-trace').start();
   require('@google/cloud-debug').start();
 }
 
 
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/r', errorTracker);
 
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/errortracker.js
+++ routes/errortracker.js
@@ -1,18 +1,20 @@
 /**
  * Created by rigwanzojunior on 6/5/17.
  *objects used
- * type ErrorRequestMeta struct {
+ *
+ * Supports errorRequest. Object with details of errorRequest
+ * type errorRequestMeta {
 	HTTPReferrer  string `json:"http_referrer,omitempty"`
 	HTTPUserAgent string `json:"http_user_agent,omitempty"`
     }
-
- type ErrorRequest struct {
+ Supports payload object errorEvent. Details from error instance
+ type errorRequest {
 	URL    string            `json:"url,omitempty"`
 	Method string            `json:"method,omitempty"`
-	Meta   *ErrorRequestMeta `json:"meta,omitempty"`
+	Meta   *errorRequestMeta `json:"meta,omitempty"`
  }
-
- type ErrorEvent struct {
+ Object encompassing payload written into logs
+ type errorEvent struct {
 	Application string `json:"application,omitempty"`
 	AppID       string `json:"app_id,omitempty"`
 	Environment string `json:"environment,omitempty"`
@@ -21,7 +23,7 @@
 	Message   string `json:"message,omitempty"`
 	Exception string `json:"exception,omitempty"`
 
-	Request *ErrorRequest `json:"request,omitempty"`
+	Request *errorRequest `json:"request,omitempty"`
 
 	Filename  string `json:"filename,omitempty"`
 	Line      int32  `json:"line,omitempty"`
@@ -34,30 +36,154 @@
 const projectId ='';
 const logName = 'javascript.errors';
 const express = require('express');
-const router = require('routes');
+const router = express.Router();
 const logging = require('@google-cloud/logging');
-const loggingClient = logging({
-    projectId:projectId
-
-});
-const log = loggingClient.log(logName);
 const url = require('url');
-
+//String constants for identifying error level.
+const errorLevels = {
+    Default:   "Default",
+    Debug:     "Debug",
+    Info:      "Info",
+    Notice:    "Notice",
+    Warning:   "Warning",
+    Error:     "Error",
+    Critical:  "Critical",
+    Alert:     "Alert",
+    Emergency: "Emergency"
+};
+const statusCodes = require('http-status-codes');
 
 /**
- *
+ *extract params in GET request from query and fill errorEvent object
  * @param req
  * @param res
  * @param next
  */
 function getHandler(req,res,next){
     var params = url.parse(req.url, true).query;
+    var referer = req.get('Referer').toString();
+    var resource = {
+        type:'compute.googleapis.com',
+        labels : {
+            "compute.googleapis.com/resource_type": "logger",
+            "compute.googleapis.com/resource_id":   "errors"
+        }
+    };
+    var line = params['l'];
+    var errorType = 'default';
+    var isUserError = false;
+    if(params['a'] === 1){
+        errorType = 'assert';
+        isUserError = true;
+    }
+    //log as INFO severity by default, because reports are spammy
 
+    var severity = 'INFO';
+    var level = errorLevels.Info;
+    // if request comes from the cache and thus only from valid AMP docs we log as "Error"
+    var isCdn =  false;
+    if(referer.startsWith('https://cdn.ampproject.org/') ||
+    referer.includes('cdn.ampproject.org/') ||
+    referer.includes('.ampproject.net/')) {
+        severity = 'ERROR';
+        level = errorLevels.Error;
+        errorType += '-cdn';
+        isCdn = true;
+    } else {
+        errorType += '-origin'
+    }
+    var is3p = false;
+    var runTime = params['rt'];
+    if (runTime !== '') {
+        errorType +=  '-' + runTime;
+        if(runTime === 'inabox'){
+            severity = 'ERROR';
+            level = errorLevels.Error;
+        }
+        if(runTime === '3p'){
+            is3p = true;
+
+        }
+    } else{
+        if(params['3p'] === 1){
+            is3p = true;
+            errorType += '-3p';
+        } else {
+            errorType += '-1p';
+        }
+    }
+    var isCanary = false;
+    if(params['ca'] === 1){
+        errorType += '-canary';
+        isCanary = true;
+    }
+    if(params['ex'] === 1){
+        errorType += '-expected';
+    }
+    var sample = Math.random(); 
+    var throttleRate = 0.01;
+    
+    if(isCanary){
+        throttleRate = 1.0 //explicitly log all errors
+
+    } else if(is3p) {
+        throttleRate = 0.1;
+
+    } else if(isCdn){
+        throttleRate = 0.1;
+    }
+
+    if(isUserError) {
+        throttleRate = throttleRate/10;
+    }
+
+    if (!sample <= throttleRate) {
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.writeHead(statusCodes.OK);
+        console.log("THROTTLED\n",res );
+        return;
+    }
+    var exception  = params['s'].toString();
+    var reg = new RegExp(':\d+$');
+    if(exception.match(reg)){
+        exception = exception.replace(new RegExp(':\d+$'),'');
+    }
+
+    //errorEvent object defined at the beginning.
+    var event = {
+        Message: params['m'],
+        Exception: exception,
+        Version: errorType + '-' + params['v'],
+        Environment:'prod',
+        Application : errorType,
+        AppID: projectId,
+        Filename : req.url.toString(),
+        Line : parseInt(line),
+        Classname: params['el'],
+        Severity : severity
+
+    };
+
+    // If format does not end with :\d+ truncate up to the last newline.
+    if(event.Message === '' && event.Exception === ''){
+        res.status(statusCodes.BAD_REQUEST).send({error:"One of 'message' or 'exception' must be present."});
+
+    }
+
+
+//get authentication context for logging
+    var loggingClient = logging({
+        projectId:projectId,
+        level: level,
+        time: new Date().getTime()
+
+    });
+    var log = loggingClient.log(logName);
 
 }
 
 /**
  * Receive GET requests
  **/
-router.get('/r',getHandler(req,res,next));
+router.get('/r',getHandler);
 module.exports = router;
