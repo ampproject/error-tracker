a4ebab6cf25b178c9b64665bd18e5b2b953b2452
Raphael Junior <rigwanzojunior@google.com>
2017-06-09 17:08:10 -0400

    Added the readme file and apache open source licenses to the code headers.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-/**
- * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
- *
+# Error reporting
+
+Receives error reports emitted by errors.js and send them to the
+[Google Cloud Error Logging service](https://cloud.google.com/error-reporting/).
+
+## Setup and deploy
+
+1. Enable Google Cloud Logging API.
+2. Switch app id in app.yaml to your own.
+3. Deploy.
+
+```
+$ node errortracker.js
+```
+
+[Sample URL](https://goo.gl/dKvgfk)
+
+This tool does not collect any user data or information. 
+
+Copyright 2016 The AMP HTML Authors. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,20 +29,3 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * Stub out Math.Random for testing
- */
-
-const express = require('express');
-const router = express.Router();
-let randomVal = 1;
-
-function random() {
-    return randomVal;
-}
-
-module.exports ={
-    router:router,
-    randomVal:randomVal,
-    random:random
-};
-
--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 /**
+ * Copyright 2016 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * App.js file that handles routing and basic error handling
- * @type {*}
  */
 let express = require('express');
 let router = express.Router();
@@ -16,11 +28,8 @@ if (process.env.NODE_ENV === 'production') {
 
 let app = express();
 
-//app.use(router);
+app.use(router);
 app.get('/r', errorTracker);
-// app.get('/r',function (req,res) {
-//     res.json({foo: 'bar'});
-// });
 
 
 // error handler
@@ -28,10 +37,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.write('error');
+  next();
 });
 
 //app.listen(3000);
