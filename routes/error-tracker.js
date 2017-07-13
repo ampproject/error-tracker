5022f7db9fb328348a3ba2c1285748c51c57b460
Raphael Junior Owino <raphnano@gmail.com>
2017-07-13 14:16:17 -0400

    Debugging travis memory error

--- routes/error-tracker.js
+++ routes/error-tracker.js
@@ -21,7 +21,7 @@
 const winston = require('winston');
 const statusCodes = require('http-status-codes');
 const unminify = require('../utils/unminify');
-const url = require('url');
+// const url = require('url');
 const logging = require('@google-cloud/logging');
 const appEngineProjectId = 'amp-error-reporting';
 const logName = 'javascript.errors';
