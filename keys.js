d78d164f0dbd92d0168920ffcac63cd4428239b7
Ryan Cebulko <ryan@cebulko.com>
2020-03-23 17:53:03 -0400

    Rewrite key download script with key storage

--- keys.js
+++ keys.js
@@ -4,37 +4,23 @@
  */
 
 const fs = require('fs');
+const { keys } = require('./utils/key-storage');
 
-const keys = ['amp-error-reporting-ads.json', 'amp-error-reporting-users.json'];
-if (keys.every(k => fs.existsSync(k))) {
+const missingKeys = keys.filter(ks => !fs.existsSync(ks.keyFilename));
+if (!missingKeys.length) {
   process.exit(0);
 }
 
-const { Storage } = require('@google-cloud/storage');
-const gcs = new Storage({
-  projectId: 'amp-error-reporting',
-});
-const bucket = gcs.bucket('amp-error-reporting.appspot.com');
-
-const downloads = keys.map(key => {
-  if (fs.existsSync(key)) {
-    return;
+const downloads = missingKeys.map(async keyStorage => {
+  try {
+    await keyStorage.downloadToFile(keyStorage.keyFilename);
+    return true;
+  } catch (error) {
+    console.error(`Error downloading ${keyStorage.keyFilename}`);
+    console.error(error);
+    fs.unlinkSync(keyStorage.keyFilename);
+    return false;
   }
-
-  return bucket
-    .file(key)
-    .download({ destination: key })
-    .then(
-      () => {
-        return true;
-      },
-      error => {
-        console.error(`Error downloading ${key}`);
-        console.error(error);
-        fs.unlinkSync(key);
-        return false;
-      }
-    );
 });
 
 Promise.all(downloads).then(statuses => {
