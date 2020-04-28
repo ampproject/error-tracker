32bdc7b9c1806460d94b5ecddabfb6b239eab842
Ryan Cebulko <ryan@cebulko.com>
2020-04-28 13:50:38 -0400

    Update check for expected log name
    Co-Authored-By: Justin Ridgewell <justin@ridgewell.name>

--- utils/log-target.js
+++ utils/log-target.js
@@ -83,7 +83,7 @@ module.exports = class LoggingTarget {
     const name = [CDN_REGEX.test(referrer) ? 'CDN' : 'Origin'];
     name.push(CHANNEL_TYPES[rtvPrefix] || 'Unknown');
 
-    if (expected && [logs.ads, logs.users].includes(this.getLog())) {
+    if (expected && this.getLog() !== logs.expected) {
       // Expected errors are split out of the main bucket, but are present for
       // user and inabox errors.
       name.push('(Expected)');
